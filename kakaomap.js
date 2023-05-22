5b8bba78be93d6b249c14b2e78b21ffcb3c10e50
always97 <always1997@kakao.com>
2023-05-23 00:01:40 +0900

    카카오맵연결

---
+++ kakaomap.js
@@ -0,0 +1,29 @@
+var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+var options = { //지도를 생성할 때 필요한 기본 옵션
+	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+	level: 3 //지도의 레벨(확대, 축소 정도)
+};
+
+container.innerHTML = "tewte"
+
+var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+
+// 마커 만들기 ***************************************************************
+var imageSrc = 'https://cdn-icons-png.flaticon.com/128/2250/2250334.png', // 마커이미지의 주소입니다    
+    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+      
+// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+    markerPosition = new kakao.maps.LatLng(33.450701, 126.570666); // 마커가 표시될 위치입니다
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition, 
+    image: markerImage // 마커이미지 설정 
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);  
+
